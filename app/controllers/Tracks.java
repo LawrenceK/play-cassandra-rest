95ae84f88c953a8432b5fa5657addf09ff35e3ea
LawrenceK <github@lklyne.co.uk>
2014-02-21 21:52:41 +0000

    index, show methods using Promise to get async database access

--- app/controllers/Tracks.java
+++ app/controllers/Tracks.java
@@ -3,33 +3,34 @@ package controllers;
 import java.util.List;
 import java.util.UUID;
 
-import play.*;
 import play.libs.Json;
 import play.mvc.*;
 import play.libs.F.*;
-import java.util.concurrent.Callable;
 import views.html.*;
 import models.Track;
 import models.TrackFactory;
-import static play.libs.F.Promise.promise;
 
 public class Tracks extends Controller
 {
 	private static TrackFactory factory = new TrackFactory();
 	
-	public static Result index() {
-//	    return ok(tracks.render(factory.findSome()));
-        return async(
-                promise(new Function0<List<Track>>() {
-                    public List<Track> apply() {
-                        return factory.findSome();
-                    }
-                }).map(new Function<List<Track>,Result>() {
-                    public Result apply(List<Track> t) {
-                    return ok(tracks.render(t));
-                    }
-                })
-            );	}
+	public static Promise<Result> index() {
+		// we do this as the data access layer uses blocking calls.
+  	  	Promise<List<Track>> findSome = Promise.promise(
+      	    new Function0<List<Track>>() {
+      	      public List<Track> apply() {
+      	        return factory.findSome();
+      	      }
+      	    }
+      	  );
+  	  	return findSome.map(
+      	    new Function<List<Track>, Result>() {
+      	      public Result apply(List<Track> t) {
+                  return ok(tracks.render(t));
+      	      }
+      	    }
+      	  );        
+	}
 
 	public static Result create() {
 		Track newTrack = Json.fromJson(request().body().asJson(), Track.class);
@@ -37,7 +38,7 @@ public class Tracks extends Controller
 		return ok(track.render(newTrack));
 	}
 
-	public static Result update(String id) {
+	public static Promise<Result> update(String id) {
 		Track dbTrack = factory.findById(UUID.fromString(id));
 		Track newTrack = Json.fromJson(request().body().asJson(), Track.class);
 	    dbTrack.update(newTrack); // some model logic you would write to do a safe merge
@@ -50,26 +51,21 @@ public class Tracks extends Controller
 		return noContent();
 	}
 
-	public static Result show(String id)  {
-		Track tr = factory.findById(UUID.fromString(id));
-		return ok(track.render(tr));
+	public static Promise<Result> show(final String id)  {
+		// we do this as the data access layer uses blocking calls.
+	  	Promise<Track> find = Promise.promise(
+	        	    new Function0<Track>() {
+	        	      public Track apply() {
+	        	        return factory.findById(UUID.fromString(id));
+	        	      }
+	        	    }
+	        	  );
+	  	return find.map(
+	        	    new Function<Track, Result>() {
+	        	      public Result apply(Track t) {
+	                    return ok(track.render(t));
+	        	      }
+	        	    }
+	        	  );        
 	}
 }
-/*
-public static Promise<Result> myAsyncAction() {
-  Promise<Integer> promiseOfInt = Promise.promise(
-    new Function0<Integer>() {
-      public Integer apply() {
-        return intensiveComputation();
-      }
-    }
-  );
-  return promiseOfInt.map(
-    new Function<Integer, Result>() {
-      public Result apply(Integer i) {
-        return ok("Got result: " + i);
-      }
-    }
-  );
-}
-*/
